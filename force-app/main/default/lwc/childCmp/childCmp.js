ebe4bf1bce868f478c26d8df778f8712347cdac4
Himanshi Shukla <himanshi.talk@gmail.com>
2024-09-13 15:44:26 -0700

    @api usage in child component

--- force-app/main/default/lwc/childCmp/childCmp.js
+++ force-app/main/default/lwc/childCmp/childCmp.js
@@ -1,5 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 
 export default class ChildCmp extends LightningElement {
-    firstName = 'Himanshi';
+    @api firstName = 'Baby';
+
+    lowerCaseLastName = 'deafult Value';
+
+    @api
+    get lastName() {
+        return this.lowerCaseLastName.toUpperCase();
+    }
+
+    set lastName(value) {
+        this.lowerCaseLastName = value;
+    }
 }

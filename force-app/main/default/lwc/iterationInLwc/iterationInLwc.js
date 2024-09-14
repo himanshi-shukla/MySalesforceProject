6e00a96352e2e6655bbc90723322fea832ffb6e3
Himanshi Shukla <himanshi.talk@gmail.com>
2024-09-13 19:04:34 -0700

    Iterations, loops in LWC HTML

---
+++ force-app/main/default/lwc/iterationInLwc/iterationInLwc.js
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+
+export default class IterationInLwc extends LightningElement {
+    contacts = [
+        {
+            Id: 1,
+            name: "Kalpana Shukla",
+            title: 'CEO',
+        },
+        {
+            Id: 2,
+            name: "Aayushi Shukla",
+            title: 'Designer',
+        }
+    ]
+}

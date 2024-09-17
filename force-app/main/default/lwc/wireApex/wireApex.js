75db454fa2542ef9310f42c16c03a900078d949e
Himanshi Shukla <himanshi.talk@gmail.com>
2024-09-16 19:37:37 -0700

    updated with error handling

--- force-app/main/default/lwc/wireApex/wireApex.js
+++ force-app/main/default/lwc/wireApex/wireApex.js
@@ -1,6 +1,18 @@
 import { LightningElement, wire } from 'lwc';
 import getContactList from '@salesforce/apex/ContactController.getContactList';
 export default class WireApex extends LightningElement {
-    @wire(getContactList)
+    //@wire(getContactList)
+    //contacts;
     contacts;
+    error;
+    @wire(getContactList)
+    wiredContact({ error, data }) {
+        if (data) {
+            this.contacts = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.contacts = undefined;
+        }
+    }
 }

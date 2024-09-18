cd98cdefabd55b2a254d78bdd6b0cac5e5918236
Himanshi Shukla <himanshi.talk@gmail.com>
2024-09-17 18:52:35 -0700

    Imperative calls to LWC

--- force-app/main/default/lwc/wireApex/wireApex.js
+++ force-app/main/default/lwc/wireApex/wireApex.js
@@ -5,6 +5,8 @@ export default class WireApex extends LightningElement {
     //contacts;
     contacts;
     error;
+
+    /*Wire with function
     @wire(getContactList)
     wiredContact({ error, data }) {
         if (data) {
@@ -14,5 +16,26 @@ export default class WireApex extends LightningElement {
             this.error = error;
             this.contacts = undefined;
         }
+    }*/
+
+    /*using conncated callback
+    connectedCallback() {
+        getContactList()
+            .then(result => {
+                this.contacts = result;
+            })
+            .catch(error => {
+                this.error = error;
+            });
+    }*/
+    // approach 4 : Event Triggered Imperative calling
+    handleLoad() {
+        getContactList()
+            .then(result => {
+                this.contacts = result;
+            })
+            .catch(error => {
+                this.error = error;
+            })
     }
 }
